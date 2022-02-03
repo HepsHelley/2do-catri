88183ee11c58e00daade42150b62837f41ad301e
HepsHelley <95265714+HepsHelley@users.noreply.github.com>
2022-02-02 23:36:22 -0600

    examen

--- 2 cuatri/lista.py
+++ 2 cuatri/lista.py
@@ -1,84 +1,33 @@
-class Person:
-    def __init__(self,nombre,edad,genero,estatura):
-        self.nombre = nombre
-        self.edad = edad
-        self.genero = genero
-        self.estatura = estatura
-def persona():
-    nombre = input("Ingresa un nombre: ")
-    nombres.append(nombre)
-    edad = input("ingresa la edad: ")
-    edades.append(edad)
-    genero = input ("ingresa el genero: ")
-    generos.append(genero)
-    estatura = input ("ingrese la estatura: ")
-    estaturas.append(estatura)
-    person = Person(nombre,edad,genero,estatura)
-    main()
-def agregar_persona():
-    nombre = input("Ingresa un nombre: ")
-    nombres.append(nombre)
-    edad = input("ingresa la edad: ")
-    edades.append(edad)
-    genero = input ("ingresa el genero: ")
-    generos.append(genero)
-    estatura = input ("ingrese la estatura: ")
-    estaturas.append(estatura)
-def ver_lista():
-    print ("que datos quiere ver? ")
-    posicion = input (" ")
-    if posicion == "0":
-        print ("nombre " + nombres[0],
-                "edad " + edades[0], 
-                "genero " + generos[0], 
-                "altura " + estaturas[0])
-    elif posicion == "1":
-       print ("nombre " + nombres[1],
-                "edad " + edades[1], 
-                "genero " + generos[1], 
-                "altura " + estaturas[1])
-    elif posicion == "2":
-        print ("nombre " + nombres[2],
-                "edad " + edades[2], 
-                "genero " + generos[2], 
-                "altura " + estaturas[2])
-    elif posicion == "3":
-        print ("nombre " + nombres[3],
-                "edad " + edades[3], 
-                "genero " + generos[3], 
-                "altura " + estaturas[3])   
-def main():
-    while True:
-        print("""
-        
-        1.- agregar a una persona
-        
-        2.- cambiar las edades
+while True:
+    x = int(input("seleccione un valor entero que sea inpar: "))
+    y = x
+    val = False
+    if x % 2 != 0: #esto es facil, cualquier numero que sea par no pasara por aquí
 
-        3.- mostrar la lista
+        matriz = [[False for i in range(x)] for j in range(y)]
+  
+        def me_avente_como_3_videos_de_un_indus_explicando_esta_madre():#por lo que entiendo ya que pues al final muchas personas me explicaron es que
+            for i in range(y):                                          #esto es para que se haga la matriz principal donde solo saldra false pero en vez
+                for j in range(x):                                      #de una lista saldra en una torrecita
+                    print(matriz[i][j], end=" ")
+                print()                                                
+        def estoy_cansado_y_me_quiero_dormir():
+            for i in range(y):                                          #aquí mas que nada ya modifico los valores y en que posicion se modificaran
+                for j in range(x):                                      #más que nada en donde esta el if hace que todas mis cordenadas que sean
+                    if i == j:                                           #iguales se ponga un True que esto sirve para hacer la linea 
+                        matriz[i][j] = True
+        estoy_cansado_y_me_quiero_dormir(),
+        me_avente_como_3_videos_de_un_indus_explicando_esta_madre()
+    elif x == 69:
+        print ("felicidades, has encontrado el easter egg, ahora cerrare el codigo por chistoso/chale no funciona :c")
+        break
+    else:
+        print ("el numero no es impar")
+        continue
+#al final digo si entiendo que hace el codigo pero no se si es el cansancio o de plano mi logica de programador es basura porque
+#aunque sepa esto no se me ocurre como hacer la cruz, supongo que con un for, saco la cantidad de numeros y dividiendolo entre 2
+#puedo conseguir la cordenada para que así se pueda hacer la cruz en vertical y horizontalmente, pero le digo que la verdad,
+#estoy cansado y solo quiero descansar profe.
 
-        4.- auto destruccion
 
-        """)
-        seleccion = input (" ")
-        if seleccion == "1":
-            agregar_persona()
-        elif seleccion == "2":
-            print ("chale aun no se usar el for")
-        elif seleccion == "3":
-            ver_lista()
-        elif seleccion == "4":
-            print ("seguro que quieres hacer eso? y/n")
-            seguro = input (" ")
-            if seguro == "y":
-                print("has escogido la muerte")
-                break
-            elif seguro =="n":
-                print ("viviremos otro dia")
-                continue
 
-nombres = []
-edades = []
-generos = []
-estaturas = []
-persona()
